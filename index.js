7a0d1c5225e80cfb789e994a51dd10b10dd27ec3
Lucas Hong <lucasjhong@gmail.com>
2022-01-28 05:10:55 -0500

    removed test route

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use('/', (req, res) => {
-	res.send('hello');
-});
-
 app.use('/people', peopleRoutes);
 app.use('/planets', planetRoutes);
 app.use('/starships', starshipRoutes);
